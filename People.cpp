c86e862f77ffc47e8d6b56be542eae12b5a56b1d
sr7539wx <sr7539wx@go.minnstate.edu>
2025-12-03 12:12:45 -0600

    People.cpp

---
+++ People.cpp
@@ -0,0 +1,19 @@
+#include "People.h"
+
+People::People() {}
+
+People::People(string n, Date d) : name(n), birthday(d) {}
+
+string People::getName() const { return name; }
+Date People::getBirthday() const { return birthday; }
+
+void People::setName(string n) { name = n; }
+void People::setBirthday(Date d) { birthday = d; }
+
+bool People::operator<(const People& rhs) const {
+    return name < rhs.name;
+}
+
+bool People::operator==(const People& rhs) const {
+    return name == rhs.name && birthday == rhs.birthday;
+}
